e99d4063942f72f14b451857269064538be050b0
中村俊介 <nakamura@feliseed.co.jp>
2023-06-07 11:08:28 +0900

    prac23

--- src/main/java/DateUtil.java
+++ src/main/java/DateUtil.java
@@ -1,25 +1,8 @@
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class DateUtil {
-
-    /**
-     * 指定日が月曜から金曜かどうかを返す
-     *
-     * @param yyyymmdd format "YYYYMMDD".
-     * @return {@code true} もし引数の日が月曜から金曜なら, {@code false} その他
-     * @throws ParseException 日付がYYYYMMDDの形式でない時
-     */
     public static boolean isWeekDay(String yyyymmdd) throws ParseException {
         final Calendar calendar = getCalendarFor(yyyymmdd);
         final int TUESDAY_CODE = 2;
@@ -35,53 +18,7 @@ public class DateUtil {
     public static boolean isSaturdayOrSunday(String yyyymmdd) throws ParseException {
         return !isWeekDay(yyyymmdd);
     }
-    public static String[] getNationalHoliday(int yyyy) throws URISyntaxException, IOException, InterruptedException  {
-        final String responseBody = getResponseBodyTo("https://holidays-jp.github.io/api/v1/" + yyyy + "/date.json");
-        final ObjectMapper objectMapper = new ObjectMapper();
-        final JsonNode jsonNode = objectMapper.readTree(responseBody);
-        return getKeyStringArr(jsonNode);
-    }
 
-    private static String[] getKeyStringArr(JsonNode jsonNode) {
-        final int size = jsonNode.size();
-        final String[] arr = new String[size];
-        final Iterator<String> iterator = jsonNode.fieldNames();
-        for (int i = 0; iterator.hasNext(); i++) {
-            arr[i] = iterator.next().replace("-", "/");
-        }
-        return arr;
-    }
-
-    public static String getResponseBodyTo(String url) throws URISyntaxException, IOException, InterruptedException {
-        final HttpClient client = HttpClient.newHttpClient();
-        final HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI(url))
-                .build();
-        final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
-    }
-
-    public static boolean isNationalHoliday(String yyyymmdd) throws URISyntaxException, IOException, InterruptedException {
-        final int year = getYearOf(yyyymmdd);
-        final String[] nationalHolidays = getNationalHoliday(year);
-        final ArrayList<String> arrDates = new ArrayList<>(Arrays.asList(nationalHolidays));
-        return arrDates.contains(yyyymmdd);
-    }
-
-    private static int getYearOf(String yyyymmdd) {
-        return Integer.parseInt(yyyymmdd.substring(0, 4));
-    }
-
-    private static Calendar getCalendarFor(String yyyymmdd) throws ParseException {
-        final Date date = DateUtil.validateAndParseDate(yyyymmdd);
-        final Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        return calendar;
-    }
-
-    public static boolean isHoliday(String yyyymmdd) throws URISyntaxException, IOException, InterruptedException, ParseException {
-        return isNationalHoliday(yyyymmdd) || isSaturdayOrSunday(yyyymmdd);
-    }
 
     public static String toYYYYMMDD(Calendar c) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
@@ -90,41 +27,38 @@ public class DateUtil {
     }
 
 
+    private static Calendar getCalendarFor(String yyyymmdd) throws ParseException {
+        final Date date = DateUtil.validateAndParseDate(yyyymmdd);
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar;
+    }
+
     public static Date validateAndParseDate(String inputDate) throws ParseException {
         final SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
         sdf.setLenient(false);
         return sdf.parse(inputDate);
     }
 
-    public static Date addDaysToDate(Date date, int days) {
-        final Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.add(Calendar.DAY_OF_MONTH, days);
-        return calendar.getTime();
-    }
-
-    public static int countWorkingDays(String from, String to) throws URISyntaxException, IOException, InterruptedException, ParseException {
+    public static int countWorkingDays(String from, String to) throws ParseException {
         String[] daysStrings = getDaysStrBetween(from, to);
         int count = 0;
         //toの日付は含めないように変更した為、daysStrings.length - 1までloop
         for (int i = 0; i < daysStrings.length - 1; i++) {
-            if(!isHoliday(daysStrings[i]))
+            if(!isSaturdayOrSunday(daysStrings[i]))
             count++;
         }
-        if(0 < countDaysBetween(from, to)) {
-            return count;
-        } else {
-            return -1 * count;
-        }
+        return count;
     }
 
     public static String[] getDaysStrBetween(String from, String to) throws ParseException{
-        int howManyDays = DateUtil.countDaysBetween(from, to) + 1;
+        Calendar fromC = getCalendarFor(from);
+        Calendar toC = getCalendarFor(to);
+        int howManyDays = DateUtil.countDaysBetween(fromC.getTime(), toC.getTime()) + 1;
         String[] strings = new String[howManyDays];
         int n = 0;
-        Calendar cal = getCalendarFor(from);
         while (n != howManyDays) {
-            Calendar tmp = (Calendar) cal.clone();
+            Calendar tmp = (Calendar) fromC.clone();
             tmp.add(Calendar.DAY_OF_MONTH, n);
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
             String formattedDate = sdf.format(tmp.getTime());
@@ -134,24 +68,25 @@ public class DateUtil {
         return strings;
     }
 
-    public static int countDaysBetween(String date1, String date2) throws ParseException {
-        Date d1 = validateAndParseDate(date1);
-        Date d2 = validateAndParseDate(date2);
-        long difference = d2.getTime() - d1.getTime();
+    public static int countDaysBetween(Date date1, Date date2) {
+        long difference = date2.getTime() - date1.getTime();
         return (int) ( difference / (24 * 60 * 60 * 1000));
     }
 
-    public static String getNextWorkingDayOf(String date) throws ParseException, URISyntaxException, IOException, InterruptedException {
+    //演習問題18
+    public static String getNextWorkingDayOf(String date) throws ParseException {
         Calendar dateCal = getCalendarFor(date);
         int n = 1;
         Calendar trialCal = (Calendar) dateCal.clone();
         trialCal.add(Calendar.DAY_OF_MONTH, n);
-        String yyyymmdd = toYYYYMMDD(trialCal);
-        while (isHoliday(yyyymmdd)) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        String formattedDate = sdf.format(trialCal.getTime());
+        while (isSaturdayOrSunday(formattedDate)) {
             trialCal.add(Calendar.DAY_OF_MONTH, 1);
-            yyyymmdd = toYYYYMMDD(trialCal);
+            sdf = new SimpleDateFormat("yyyy/MM/dd");
+            formattedDate = sdf.format(trialCal.getTime());
         }
-        return yyyymmdd;
+        return formattedDate;
     }
 
 }
--- src/test/java/DateUtilTest.java
+++ src/test/java/DateUtilTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
@@ -47,61 +45,6 @@ class DateUtilTest {
     }
 
     @Test
-    void getNationalHoliday() throws Exception {
-        String[] expected = {
-                "2023/01/01",
-                "2023/01/02",
-                "2023/01/09",
-                "2023/02/11",
-                "2023/02/23",
-                "2023/03/21",
-                "2023/04/29",
-                "2023/05/03",
-                "2023/05/04",
-                "2023/05/05",
-                "2023/07/17",
-                "2023/08/11",
-                "2023/09/18",
-                "2023/09/23",
-                "2023/10/09",
-                "2023/11/03",
-                "2023/11/23"
-        };
-        System.out.println("actual");
-        String[] actual = DateUtil.getNationalHoliday(2023);
-        System.out.println(actual);
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void isNationalHoliday() throws Exception {
-        assertEquals(true, DateUtil.isNationalHoliday("2023/01/01"));
-        assertEquals(true, DateUtil.isNationalHoliday("2023/01/02"));
-        assertEquals(false, DateUtil.isNationalHoliday("2023/01/03"));
-        assertEquals(false, DateUtil.isNationalHoliday("2023/01/04"));
-        assertEquals(false, DateUtil.isNationalHoliday("2023/01/05"));
-        assertEquals(false, DateUtil.isNationalHoliday("2023/01/06"));
-        assertEquals(false, DateUtil.isNationalHoliday("2023/01/07"));
-    }
-
-    @Test
-    void isHoliday() throws Exception {
-        assertEquals(true, DateUtil.isHoliday("2023/01/01"));
-        assertEquals(true, DateUtil.isHoliday("2023/01/02"));
-        assertEquals(false, DateUtil.isHoliday("2023/01/03"));
-        assertEquals(false, DateUtil.isHoliday("2023/01/04"));
-        assertEquals(false, DateUtil.isHoliday("2023/01/05"));
-        assertEquals(false, DateUtil.isHoliday("2023/01/06"));
-        assertEquals(true, DateUtil.isHoliday("2023/01/07"));
-        assertEquals(true, DateUtil.isHoliday("2023/01/08"));
-        assertEquals(true, DateUtil.isHoliday("2023/01/09"));
-        assertEquals(true, DateUtil.isHoliday("2022/01/08"));
-        assertEquals(true, DateUtil.isHoliday("2022/01/09"));
-        assertEquals(true, DateUtil.isHoliday("2022/01/10"));
-        assertEquals(false, DateUtil.isHoliday("2022/01/11"));
-    }
-
-    @Test
     void validateAndParseDate() throws ParseException {
         String inputDate = "2023-05-07";
         assertThrows(ParseException.class, () -> DateUtil.validateAndParseDate(inputDate));
@@ -114,10 +57,18 @@ class DateUtilTest {
 
     @Test
     void getDaysBetweenDates() throws ParseException {
-        assertEquals(2, DateUtil.countDaysBetween("2023/03/04", "2023/03/06"));
-        assertEquals(2, DateUtil.countDaysBetween("2023/03/30", "2023/04/01"));
-        assertEquals(5, DateUtil.countDaysBetween("2023/03/30", "2023/04/04"));
-        assertEquals(31, DateUtil.countDaysBetween("2023/03/30", "2023/04/30"));
+        Date date1_1 = DateUtil.validateAndParseDate("2023/03/04");
+        Date date1_2 = DateUtil.validateAndParseDate("2023/03/06");
+        assertEquals(2, DateUtil.countDaysBetween(date1_1, date1_2));
+        Date date2_1 = DateUtil.validateAndParseDate("2023/03/30");
+        Date date2_2 = DateUtil.validateAndParseDate("2023/04/01");
+        assertEquals(2, DateUtil.countDaysBetween(date2_1, date2_2));
+        Date date3_1 = DateUtil.validateAndParseDate("2023/03/30");
+        Date date3_2 = DateUtil.validateAndParseDate("2023/04/04");
+        assertEquals(5, DateUtil.countDaysBetween(date3_1, date3_2));
+        Date date4_1 = DateUtil.validateAndParseDate("2023/03/30");
+        Date date4_2 = DateUtil.validateAndParseDate("2023/04/30");
+        assertEquals(31, DateUtil.countDaysBetween(date4_1, date4_2));
     }
 
     @Test
@@ -155,25 +106,18 @@ class DateUtilTest {
 
     //Taskクラスとの兼ね合いで、toの日付は含めないように仕様変更
     @Test
-    void countWorkingDays() throws URISyntaxException, IOException, InterruptedException, ParseException {
+    void countWorkingDays() throws ParseException {
         assertEquals(1, DateUtil.countWorkingDays("2023/05/11", "2023/05/12"));
-        assertEquals(3, DateUtil.countWorkingDays("2023/05/11", "2023/05/16"));
-        assertEquals(7, DateUtil.countWorkingDays("2023/05/11", "2023/05/20"));
-        assertEquals(7, DateUtil.countWorkingDays("2023/05/11", "2023/05/21"));
-        assertEquals(0, DateUtil.countWorkingDays("2023/05/20", "2023/05/21"));
-        assertEquals(0, DateUtil.countWorkingDays("2023/05/21", "2023/05/21"));
-        assertEquals(0, DateUtil.countWorkingDays("2023/01/01", "2023/01/03"));
-        assertEquals(1, DateUtil.countWorkingDays("2023/01/06", "2023/01/09"));
-        assertEquals(1, DateUtil.countWorkingDays("2023/01/06", "2023/01/10"));
-        assertEquals(19, DateUtil.countWorkingDays("2023/01/01", "2023/01/31"));
-        assertEquals(5, DateUtil.countWorkingDays("2022/12/27", "2023/01/04"));
     }
 
     @Test
-    void getNextWorkingDayOf() throws Exception {
-        assertEquals("2023/06/01", DateUtil.getNextWorkingDayOf("2023/05/31"));
-        assertEquals("2023/06/05", DateUtil.getNextWorkingDayOf("2023/06/02"));
-        assertEquals("2023/01/03", DateUtil.getNextWorkingDayOf("2023/01/01"));
-        assertEquals("2023/01/10", DateUtil.getNextWorkingDayOf("2023/01/06"));
+    void getNextWorkingDayOf() throws ParseException {
+        assertEquals("2023/01/02", DateUtil.getNextWorkingDayOf("2022/12/31"));
+        assertEquals("2023/01/02", DateUtil.getNextWorkingDayOf("2023/01/01"));
+        assertEquals("2023/01/03", DateUtil.getNextWorkingDayOf("2023/01/02"));
+        assertEquals("2023/01/04", DateUtil.getNextWorkingDayOf("2023/01/03"));
+        assertEquals("2023/01/09", DateUtil.getNextWorkingDayOf("2023/01/06"));
     }
+
+
 }
